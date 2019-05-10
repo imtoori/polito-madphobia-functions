1118bc0706f16ea18b8b9772a965a49ea4826b94
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2019-05-10 14:24:09 +0200

    ADd badge

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,9 +3,11 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp(functions.config().firebase);
 
-export const onOrderCreated = functions.database.ref('/orders/{orderId}').onCreate(snapshot => {
+export const onOrderCreated = functions.database.ref('/orders/{orderId}').onCreate(async snapshot => {
   const val = snapshot.val();
   const restaurantId = val.restaurantId;
+
+  await admin.database().ref(`/users/restaurants/${restaurantId}/badge`).set(true);
   console.log(`restaurant: ${restaurantId}`);
   if (restaurantId) {
     const topic = `${restaurantId}.order.new`;
@@ -21,12 +23,13 @@ export const onOrderCreated = functions.database.ref('/orders/{orderId}').onCrea
   return Promise.resolve();
 });
 
-export const onBikerAssignedCreated = functions.database.ref('/orders/{orderId}').onUpdate(change => {
+export const onBikerAssignedCreated = functions.database.ref('/orders/{orderId}').onUpdate(async change => {
   const before = change.before.val();
   const beforeStatus = before.status;
   const after = change.after.val();
   const afterStatus = after.status;
   if (beforeStatus === 'pending' && afterStatus === 'preparing') {
+    await admin.database().ref(`/users/restaurants/${after.bikerId}/badge`).set(true);
     const topic = `${after.bikerId}.order.new`;
     console.log(`sending to topic ${topic}`);
     return admin.messaging().sendToTopic(topic, {
