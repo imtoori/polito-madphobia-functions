93b73955f98c2d6de6b3af81e277f98473d4873f
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2019-05-26 15:24:49 +0200

    Add notification to client

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,17 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp(functions.config().firebase);
 
+const sendNotification = async (topic: string, title: string) => {
+  console.log(`sending to topic ${topic}`);
+  return admin.messaging().sendToTopic(topic, {
+    notification: {
+      title: title
+    }
+  }, {
+    priority: 'high'
+  });
+};
+
 export const onOrderCreated = functions.database.ref('/orders/{orderId}').onCreate(async snapshot => {
   const val = snapshot.val();
   const restaurantId = val.restaurantId;
@@ -10,35 +21,36 @@ export const onOrderCreated = functions.database.ref('/orders/{orderId}').onCrea
   await admin.database().ref(`/users/restaurants/${restaurantId}/badge`).set(true);
   console.log(`restaurant: ${restaurantId}`);
   if (restaurantId) {
-    const topic = `${restaurantId}.order.new`;
-    console.log(`sending to topic ${topic}`);
-    return admin.messaging().sendToTopic(topic, {
-      notification: {
-        title: 'New order arrived!'
-      }
-    }, {
-      priority: 'high'
-    });
+    return sendNotification(`${restaurantId}.order.new`, 'New order arrived!');
   }
   return Promise.resolve();
 });
 
-export const onBikerAssignedCreated = functions.database.ref('/orders/{orderId}').onUpdate(async change => {
+export const onOrderUpdated = functions.database.ref('/orders/{orderId}').onUpdate(async change => {
   const before = change.before.val();
   const beforeStatus = before.status;
   const after = change.after.val();
   const afterStatus = after.status;
+  const clientId = after.client.id;
+
+  switch (afterStatus) {
+    case 'preparing':
+      await sendNotification(`${clientId}.order.status`, 'The restaurant is preparing your order!');
+      break;
+    case 'ready':
+      await sendNotification(`${clientId}.order.status`, 'Your order is ready to pick up!');
+      break;
+    case 'completed':
+      await sendNotification(`${clientId}.order.status`, 'Your order has left the restaurant!');
+      break;
+    case 'delivered':
+      await sendNotification(`${clientId}.order.status`, 'Enjoy your meal! And rate your experience!');
+      break;
+  }
+
   if (beforeStatus === 'pending' && afterStatus === 'preparing') {
     await admin.database().ref(`/users/restaurants/${after.bikerId}/badge`).set(true);
-    const topic = `${after.bikerId}.order.new`;
-    console.log(`sending to topic ${topic}`);
-    return admin.messaging().sendToTopic(topic, {
-      notification: {
-        title: 'New order arrived!'
-      }
-    }, {
-      priority: 'high'
-    });
+    return sendNotification(`${after.bikerId}.order.new`, 'New order arrived!');
   }
   return Promise.resolve();
 });
